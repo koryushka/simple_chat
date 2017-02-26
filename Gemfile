cdb89f9b867fbf4b64a2972c48940570b0982ad8
koryushka <karetnikov.kirill@gmail.com>
2017-02-26 22:15:07 +0300

    Removed byebug

--- Gemfile
+++ Gemfile
@@ -33,13 +33,11 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'haml-rails'
-gem 'byebug'
 gem 'bootstrap-sass'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
 end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
-      # byebugwhere('created_at > ?', current_user['login_time'])
-
-      @messages = Message.where(["messages.created_at > ?", current_user['login_time']]).order(created_at: :asc)
+      @messages = Message.where(["messages.created_at > ?", current_user['login_time']])
       @message = Message.new
       @user = current_user
     else
